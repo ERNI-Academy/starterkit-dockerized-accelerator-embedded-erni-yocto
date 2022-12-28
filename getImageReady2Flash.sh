902616b410ba9e851e53bf85ec3381e22ff31d50
Gonzalo Matarrubia <g.matarrubia@asysnet.es>
2022-12-28 11:15:44 +0100

    minor fix

--- getImageReady2Flash.sh
+++ getImageReady2Flash.sh
@@ -2,6 +2,6 @@
 
 currentPath="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 
-pushd raspberrypi3/tmp-glibc/deploy/images/raspberrypi3 || return
+pushd "${MACHINE}"/tmp-glibc/deploy/images/raspberrypi3 || return
 bmaptool copy core-image-minimal-raspberrypi3.wic.bz2 "${currentPath}"/disk.img
 popd || return
